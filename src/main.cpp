d25afabc8eebc75bacff8e27cc3dc9b990f18a51
DK <gottyduke@hotmail.com>
2023-08-12 02:25:20 +0800

    fix: consolidate patches

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,82 @@
 
 using namespace DKUtil::Alias;
 
-namespace
+namespace Patches
 {
 	static constexpr auto TestAlByte = dku::Hook::Assembly::make_pattern<"84 C0">();
 	static constexpr dku::Hook::OpCode Xor = 0x33;
 
+	using patch_entry = std::pair<void*, std::ptrdiff_t>;
+
 	// 4.1.1.3624901
-	void Patch1_Commit()
+	void Commit()
 	{
-		auto* entry = dku::Hook::Assembly::search_pattern<
-			"40 57 "
-			"48 83 EC 30 "
-			"48 8B 0D ?? ?? ?? ?? "
-			"48 8B FA "
-			"48 81 C1 D0 00 00 00 "
-			"E8 ?? ?? ?? ?? "
-			"48 8B C8 "
-			"E8 ?? ?? ?? ??">();
-		auto patch = AsAddress(dku::Hook::adjust_pointer(entry, 0x24));
+		patch_entry Patches[3] = {
+			{ dku::Hook::Assembly::search_pattern<
+				  "40 57 "
+				  "48 83 EC 30 "
+				  "48 8B 0D ?? ?? ?? ?? "
+				  "48 8B FA "
+				  "48 81 C1 D0 00 00 00 "
+				  "E8 ?? ?? ?? ?? "
+				  "48 8B C8 "
+				  "E8 ?? ?? ?? ??">(),
+				0x24 },
+			{ dku::Hook::Assembly::search_pattern<
+				  "40 53 "
+				  "56 "
+				  "57 "
+				  "48 81 EC D0 04 00 00 "
+				  "48 8B 05 ?? ?? ?? ?? "
+				  "48 33 C4 "
+				  "48 89 84 24 C0 04 00 00 "
+				  "48 8B D9 "
+				  "49 8B F8 "
+				  "48 8B 0D ?? ?? ?? ?? "
+				  "48 8B F2 "
+				  "48 81 C1 D0 00 00 00 "
+				  "E8 ?? ?? ?? ?? "
+				  "48 8B C8 "
+				  "E8 ?? ?? ?? ??">(),
+				0x41 },
+			{ dku::Hook::Assembly::search_pattern<
+				  "40 53 "
+				  "55 "
+				  "56 "
+				  "57 "
+				  "41 56 "
+				  "41 57 "
+				  "48 81 EC ?? ?? ?? ?? "
+				  "48 8B 05 ?? ?? ?? ?? "
+				  "48 33 C4 "
+				  "48 89 84 24 ?? ?? ?? ?? "
+				  "48 8B D9 "
+				  "45 0F B7 F9 "
+				  "48 8B 0D ?? ?? ?? ?? "
+				  "49 8B F8 "
+				  "48 81 C1 ?? ?? ?? ?? "
+				  "48 8B EA "
+				  "E8 ?? ?? ?? ?? "
+				  "48 8B C8 "
+				  "E8 ?? ?? ?? ??">(),
+				0x4A },
+		};
 
-		if (entry && TestAlByte.match(patch)) {
-			dku::Hook::WriteImm(patch, Xor);
-			INFO("patch 1 committed : {:X}", patch);
+
+		for (auto i = 0; i < std::extent_v<decltype(Patches)>; ++i) {
+			auto& [entry, offset] = Patches[i];
+
+			auto patch = AsAddress(dku::Hook::adjust_pointer(entry, offset));
+
+			if (entry && TestAlByte.match(patch)) {
+				INFO("patch {} committed at {:X}", i + 1, patch);
+				dku::Hook::WriteImm(patch, Xor);
+			} else {
+				INFO("patch {} failed", i + 1);
+			}
 		}
 	}
-
-	void Patch2_Commit()
-	{
-		auto* entry = dku::Hook::Assembly::search_pattern<
-			"40 53 "
-			"56 "
-			"57 "
-			"48 81 EC D0 04 00 00 "
-			"48 8B 05 ?? ?? ?? ?? "
-			"48 33 C4 "
-			"48 89 84 24 C0 04 00 00 "
-			"48 8B D9 "
-			"49 8B F8 "
-			"48 8B 0D ?? ?? ?? ?? "
-			"48 8B F2 "
-			"48 81 C1 D0 00 00 00 "
-			"E8 ?? ?? ?? ?? "
-			"48 8B C8 "
-			"E8 ?? ?? ?? ??">();
-		auto patch = AsAddress(dku::Hook::adjust_pointer(entry, 0x41));
-
-		if (entry && TestAlByte.match(patch)) {
-			dku::Hook::WriteImm(patch, Xor);
-			INFO("patch 2 committed : {:X}", patch);
-		}
-	}
-
-	void Patch3_Commit()
-	{
-		auto* entry = dku::Hook::Assembly::search_pattern<
-			"40 53 "
-			"55 "
-			"56 "
-			"57 "
-			"41 56 "
-			"41 57 "
-			"48 81 EC ?? ?? ?? ?? "
-			"48 8B 05 ?? ?? ?? ?? "
-			"48 33 C4 "
-			"48 89 84 24 ?? ?? ?? ?? "
-			"48 8B D9 "
-			"45 0F B7 F9 "
-			"48 8B 0D ?? ?? ?? ?? "
-			"49 8B F8 "
-			"48 81 C1 ?? ?? ?? ?? "
-			"48 8B EA "
-			"E8 ?? ?? ?? ?? "
-			"48 8B C8 "
-			"E8 ?? ?? ?? ??">();
-		auto patch = AsAddress(dku::Hook::adjust_pointer(entry, 0x4A));
-
-		if (entry && TestAlByte.match(patch)) {
-			dku::Hook::WriteImm(patch, Xor);
-			INFO("patch 3 committed : {:X}", patch);
-		}
-	}
-}
+} // namespace Patches
 
 BOOL APIENTRY DllMain(HMODULE a_hModule, DWORD a_ul_reason_for_call, LPVOID a_lpReserved)
 {
@@ -97,9 +92,7 @@ BOOL APIENTRY DllMain(HMODULE a_hModule, DWORD a_ul_reason_for_call, LPVOID a_lp
 
 		INFO("game type : {}", dku::Hook::GetProcessName());
 
-		Patch1_Commit();
-		Patch2_Commit();
-		Patch3_Commit();
+		Patches::Commit();
 	}
 
 	return TRUE;
